5c9b66f7682e222e1990c72e9d54235c61a99308
elquellegatarde <141068557+elquellegatarde@users.noreply.github.com>
2023-11-19 18:28:43 -0300

    Add files via upload

---
+++ entrega 3/script.js
@@ -0,0 +1,6 @@
+<script>
+function toggleVideo() {
+    VideoFrame = document.getElementById('videos-iframe');
+    videoIframe.style.display = (videoIframe.style.display === 'none') ? 'block' : 'none';
+}
+</script>
