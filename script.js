61199ee285d7a97bdd243adf06be3b4814a7f382
elquellegatarde <deusmoes@gmail.com>
2023-11-19 18:25:32 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+function toggleVideo() {
+    var videoIframe = document.getElementById('videos-iframe');
+    videoIframe.style.display = (videoIframe.style.display === 'none') ? 'block' : 'none';
+}
+</script>
